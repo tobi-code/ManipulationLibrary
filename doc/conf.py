2378ca94bf89623c76a2b1e4b311e64f798b0949
tobias.struebing <tobias.struebing@stud.uni-goettingen.de>
2020-07-22 14:57:58 +0200

    Edited Readme

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
