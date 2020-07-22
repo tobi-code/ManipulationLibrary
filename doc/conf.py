f176312d6ac0fd34491dcb0ceec5144f81c3fa69
tobias.struebing <tobias.struebing@stud.uni-goettingen.de>
2020-07-22 15:24:34 +0200

    .

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-autodoc_mock_imports = ["tabula", "pandas", "numpy"]
+autodoc_mock_imports = ["tabula", "pandas", "numpy", "matplotlib", "scipy", "itertools", "seaborn"]
 
 # -- Project information -----------------------------------------------------
 
