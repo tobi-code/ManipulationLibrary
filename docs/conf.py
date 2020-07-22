61576f5c9f0d05cf4b2a84c4354420fea6b00e32
tobias.struebing <tobias.struebing@stud.uni-goettingen.de>
2020-07-22 15:51:52 +0200

    .

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,8 @@ sys.path.insert(0, os.path.abspath('..'))
 current_file_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(
     0,
-    os.path.join(current_file_dir, "..", "build", "lib", "python_package"),
+    os.path.join(current_file_dir, "..", "build", "lib", "python_package"
+                 ),
 )
 
 # -- Project information -----------------------------------------------------
