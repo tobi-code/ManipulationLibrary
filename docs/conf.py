d373f040933f8c1ebacc6b5de35ad1023f0c06a1
tobias.struebing <tobias.struebing@stud.uni-goettingen.de>
2020-11-05 14:53:37 +0100

    Added manipulation_analyser

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.append('/home/sazu/ownCloud/ManipulationLibrary/')
-autodoc_mock_imports = ["tabula", "pandas", "numpy", "matplotlib", "scipy", "itertools", "seaborn", "open3d"]
+autodoc_mock_imports = ["tabula", "pandas", "numpy", "matplotlib", "scipy", "itertools", "seaborn", "open3d", "cv2"]
 
 # -- Project information -----------------------------------------------------
 
