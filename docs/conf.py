d320bbdfddb95acd892b2269a0c38c06371f6b73
tobias.struebing <tobias.struebing@stud.uni-goettingen.de>
2020-11-05 14:46:20 +0100

    Added manipulation_analyser

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,13 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
