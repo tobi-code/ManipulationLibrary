fe0e98344b6d8941293a4b02041e0b628a9673d3
tobi-code <tobias.struebing@stud.uni-goettingen.de>
2021-03-29 18:52:46 +0200

    Updated documentation

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 import mock
 
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append('../../ManipulationLibrary/')
